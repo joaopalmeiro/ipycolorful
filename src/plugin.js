6acb5773af015d543961452762450fc6aa32a4d0
João Palmeiro <joaommpalmeiro@gmail.com>
2022-03-07 18:56:37 +0000

    Update boilerplate

---
+++ src/plugin.js
@@ -0,0 +1,22 @@
+import { IJupyterWidgetRegistry } from "@jupyter-widgets/base";
+import { MODULE_NAME, MODULE_VERSION } from "./version";
+import * as widgetExports from "./widget";
+
+const EXTENSION_ID = "ipycolorful:plugin";
+
+function activateWidgetExtension(app, registry) {
+  registry.registerWidget({
+    name: MODULE_NAME,
+    version: MODULE_VERSION,
+    exports: widgetExports,
+  });
+}
+
+const plugin = {
+  id: EXTENSION_ID,
+  requires: [IJupyterWidgetRegistry],
+  activate: activateWidgetExtension,
+  autoStart: true,
+};
+
+export default plugin;
