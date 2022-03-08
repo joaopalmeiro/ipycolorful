5b0d8a041047bdcf27fa27188cd7e8ed0a3b6b0e
João Palmeiro <joaommpalmeiro@gmail.com>
2022-03-08 20:37:16 +0000

    Add tsc and demo notebook

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
-import metadata from "../package.json";
-// const metadata = require("../package.json");
+const metadata = require("../package.json");
+// import metadata from "../package.json";
 
 export const MODULE_VERSION = metadata.version;
 export const MODULE_NAME = metadata.name;
