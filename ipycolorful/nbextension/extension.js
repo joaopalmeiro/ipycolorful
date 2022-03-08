895bbf88cf117c3a4c9eea5cda95b7b7d999c26f
João Palmeiro <joaommpalmeiro@gmail.com>
2022-03-08 19:54:44 +0000

    Update boilerplate

---
+++ ipycolorful/nbextension/extension.js
@@ -0,0 +1,16 @@
+// Entry point for the notebook bundle containing custom model definitions.
+define(function () {
+  "use strict";
+
+  window["requirejs"].config({
+    map: {
+      "*": {
+        ipycolorful: "nbextensions/ipycolorful/index",
+      },
+    },
+  });
+  // Export the required load_ipython_extension function
+  return {
+    load_ipython_extension: function () {},
+  };
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ const version = require("./package.json").version;
 // - https://github.com/bqplot/bqplot/blob/master/js/webpack.config.js
 // - https://webpack.js.org/configuration/resolve/#resolveextensions
 // - https://github.com/bqplot/bqplot/blob/master/js/webpack.lab.config.js
+// - https://stackoverflow.com/a/42672703
 
 const rules = [{ test: /\.jsx$/, loader: "babel-loader" }];
 
+// Packages that shouldn't be bundled but loaded at runtime.
 const externals = ["@jupyter-widgets/base"];
 
 const resolve = {
